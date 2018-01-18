40854b14677dcdf8123def50efc23f231c238bb4
AiroHass <airo.hass@khk.ee>
2018-01-18 09:41:52 +0200

    Valmistatud conf. fail ning defineeritud esimesed abikonstandid.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Programmeerimisvahendite kasutamine
-======================
+
 Antud rakendus on koostatud antud aine raames
 ja on mõeldud MVC rakenduse koostamise mudeli õppimiseks.
 ning PHP oskuste arendamiseks ja laiendamiseks  OOP osas.
---
+++ conf.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: airo.hass
+ * Date: 18.01.2018
+ * Time: 9:34
+ */
+// konfiguratsiooni fail
+
+//abikonstandid
+define('MODEL_DIR', 'model/');
+define('VIEWS_DIR', "VIEWS/");
+define('CONTROL_DIR','controllers/');
