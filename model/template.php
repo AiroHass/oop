4de73d854e88dd38605e23632c0004337dda2c26
AiroHass <airo.hass@khk.ee>
2018-01-18 10:29:59 +0200

    Template omadused on määratud

---
+++ model/template.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: airo.hass
+ * Date: 18.01.2018
+ * Time: 10:21
+ */
+
+class template
+{
+    var $file = ''; // html vaate faili nimi
+    var $content = false; // sisu deafultina pole, HTML faili sisu
+    var $vars = array(); // tulevased väärtused
+}
