18aab9665b58ed8476d24e6f768c4acad632dcf0
AiroHass <airo.hass@khk.ee>
2018-01-18 10:40:14 +0200

    Sisu lugemine failist. Mitu varianti.

--- model/template.php
+++ model/template.php
@@ -11,4 +11,12 @@ class template
     var $file = ''; // html vaate faili nimi
     var $content = false; // sisu deafultina pole, HTML faili sisu
     var $vars = array(); // tulevased väärtused
+    //template klassi meetodid
+    // Sisu lugemine
+    function readFile($f) {
+//        $fp = fopen($f, 'r');
+//        $this -> content = fread($fp, filesize($f));
+//        fclose($fp);
+        $this-> content=file_get_contents($f);
+    }
 }
